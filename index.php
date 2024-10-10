6c2d517b26ed64b60dabecba41d6c9c1ee82e15b
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-10-10 08:57:04 -0600

    Conexion a la BD

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
                     <?php }?>
 
                     <li class="nav-item">
-                        <button class="nav-link click-scroll" name="cerrarSesion" id="cerrarSesion">Salir</button>
+                        <button type="button" class="nav-link click-scroll" name="cerrarSesion" id="cerrarSesion">Salir</button>
                     </li>
 
                 </ul>
