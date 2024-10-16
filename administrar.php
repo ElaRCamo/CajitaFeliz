9e44f622d46fa10a1b8b89dc9970616eb8603e21
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-10-16 11:25:06 -0600

    Apartado Solicitudes Prestamos Admin

--- administrar.php
+++ administrar.php
@@ -118,9 +118,7 @@
                             <th>Acciones</th>
                         </tr>
                         </thead>
-                        <tbody id="bodyPrestamosAdmin">
-
-                        </tbody>
+                        <tbody id="bodyPrestamosAdmin"></tbody>
                     </table>
 
                 </div>
@@ -253,7 +251,7 @@
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-
+<script src="js/administrador.js"></script>
 </body>
 
 </html>
