bf2c77e7598152cca79d858f1b52c0f25d772af1
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-10-16 11:49:09 -0600

    Apartado Solicitudes Prestamos Admin

--- administrar.php
+++ administrar.php
@@ -206,6 +206,7 @@
 <!-- JavaScript para exportar la tabla a Excel -->
 <script>
     window.addEventListener("load",async () => {
+        alert("cargando ");
         await initDataTablePresAdmin;
     })
 
@@ -247,12 +248,13 @@
 <script src="js/jquery.sticky.js"></script>
 <script src="js/counter.js"></script>
 <script src="js/custom.js"></script>
+<script src="js/administrador.js"></script>
 <!-- DataTable -->
 <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-<script src="js/administrador.js"></script>
+
 </body>
 
 </html>
