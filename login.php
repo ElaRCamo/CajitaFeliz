bee8945dd458b5d186890eacc7addf9931b47ac4
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-10-23 15:27:06 -0600

    Styles login

--- login.php
+++ login.php
@@ -21,43 +21,34 @@
 </head>
 <body>
     <main>
-
-        <div class="container" >
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-6" id="form-container">
-                            <div class="col-lg-6 col-12 " id="login-container">
-                                <small><strong>Cajita Feliz Grammer</strong></small>
-                                <div class="wrapper">
-                                    <form id="formInicioSesion" action="dao/daoLogin.php" method="post"  >
-                                        <h2 id="iniciarSesion"><br>¡Hola,</h2>
-                                        <h2 ><strong>Bienvenido!</strong></h2>
-                                        <div class="input-box form-group" id="userDiv">
-                                            <label for="numNomina">Nómina</label>
-                                            <input type="text" class="form-control" name="numNomina" id="numNomina" placeholder="No. de nómina" required data-error="Ingrese un número de nómina válido.">
-                                            <i class="las la-user"></i>
-                                            <div class="invalid-feedback"></div>
-                                        </div>
-                                        <div class="input-box form-group" id="tagDiv">
-                                            <label for="password">TAG</label>
-                                            <input type="password" class="form-control" name="password"  id="password" placeholder="TAG" required data-error="Ingrese un TAG válido.">
-                                            <i class="las la-lock"></i>
-                                            <div class="invalid-feedback"></div>
-                                        </div>
-                                        <button type="submit" class="btn login" name="iniciarSesionBtn">Iniciar Sesión</button>
-                                    </form>
-                                    <div>
-                                        <label>Siguenos</label>
-                                        <a href="">facebook</a>
-                                    </div>
-                                </div>
-                            </div>
+        <div class="container container-center">
+            <div class="rounded-div shadow">
+                <div class="left-side">
+                    <small><strong>Cajita Feliz Grammer</strong></small>
+                    <h2 id="iniciarSesion"><br>¡Hola,</h2>
+                    <h2 ><strong>Bienvenido!</strong></h2>
+                    <form id="formInicioSesion" action="dao/daoLogin.php" method="post"  >
+                        <div class="input-box form-group" id="userDiv">
+                            <label for="numNomina">Nómina</label>
+                            <input type="text" class="form-control" name="numNomina" id="numNomina" placeholder="No. de nómina" required data-error="Ingrese un número de nómina válido.">
+                            <i class="las la-user"></i>
+                            <div class="invalid-feedback"></div>
                         </div>
-                        <div class="col-6" id="conteinerIMG">
-                                <img src="images/slide/pigCajita.png" id="imgLogin" class="img-fluid" alt="CajitaFuerte">
+                        <div class="input-box form-group" id="tagDiv">
+                            <label for="password">TAG</label>
+                            <input type="password" class="form-control" name="password"  id="password" placeholder="TAG" required data-error="Ingrese un TAG válido.">
+                            <i class="las la-lock"></i>
+                            <div class="invalid-feedback"></div>
                         </div>
+                        <button type="submit" class="btn login" name="iniciarSesionBtn">Iniciar Sesión</button>
+                    </form>
+                    <div>
+                        <label>Siguenos</label>
+                        <a href="">facebook</a>
                     </div>
+                </div>
+                <div class="right-side">
+                    <!-- La parte derecha tendrá la imagen de fondo -->
                 </div>
             </div>
         </div>
