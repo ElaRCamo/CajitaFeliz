683fb0f3c9410196c7798c961498237ad8d89e7b
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-01 12:09:16 -0600

    styles misSolicitudes

--- js/administrador.js
+++ js/administrador.js
@@ -5,7 +5,7 @@ let dataTableInitPrestamosAdmin = false;
 const dataTableOptPresAdmin = {
     lengthMenu: [5, 10, 15, 20],
     columnDefs:[
-        {className: "centered", targets: [0,1,2,3,4,6,6]},
+        {className: "centered", targets: [0,1,2,3,4,5,6]},
         {orderable: false, targets: [0,1,2]},
         {width: "8%", targets: [0]},
         {searchable: true, targets: [0,1,2,3] }
