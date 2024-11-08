b468b4a93b6aceb963182fb4e4da8b6e7cc70ba8
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-08 12:06:06 -0600

    modal read retiro

--- dao/daoRetiroPorId.php
+++ dao/daoRetiroPorId.php
@@ -19,9 +19,9 @@ function obtenerRetiroPorId($idSolicitud){
                     c.nomina as usuario,
                     CASE
                         WHEN r.estatusRetiro = 0
-                            THEN CONCAT('<span class=\"badge bg-warning text-dark\" title=\"En proceso\">En proceso</span>')
+                            THEN CONCAT('<label class=\"badge bg-warning text-dark\" title=\"En proceso\">En proceso</label>')
                         WHEN r.estatusRetiro = 1
-                            THEN CONCAT('<span class=\"badge bg-success\" title=\"Completado\">Completado</span>')
+                            THEN CONCAT('<label class=\"badge bg-success\" title=\"Completado\">Completado</label>')
                     END AS estatusVisual
                 FROM
                     RetiroAhorro r, CajaAhorro c
